8c09f2bec5d790aa02fda365ade0487e037beb76
Max Boyar <max.boyar.a@gmail.com>
2021-09-02 12:03:28 +0300

    Init

---
+++ check.php
@@ -0,0 +1,95 @@
+<?php
+require  __DIR__ . '/vendor/autoload.php';
+
+
+$routers = [
+  ['host' => '', 'username'=> '', 'password' => '', 'port' => 55055]
+];
+
+$lastStates = [];
+$currentStates = [];
+
+//Load last state
+if(file_exists(__DIR__ . '/last_state.json')) {
+    $state = json_decode(file_get_contents(__DIR__ . '/last_state.json'), true);
+    foreach ($state as $st) {
+        $lastStates["{$st['router']}{$st['remote-address']}"] = $st;
+    }
+}
+
+//Load current states from routerAPI
+foreach ($routers as $router) {
+    $api = new RouterosAPI;
+    $api->port = $router['port'];
+    if(!$api->connect($router['host'], $router['username'], $router['password'])) {
+        echo "Error connecting to {$router['host']} \n";
+        continue;
+    }
+    $peers = $api->comm('/routing/bgp/peer/print');
+    foreach ($peers as $peer) {
+        $currentStates["{$router['host']}{$peer['remote-address']}"] = [
+            'router' => $router['host'],
+            'name' => $peer['name'],
+            'local-address' => isset($peer['local-address']) ? $peer['local-address'] : null,
+            'remote-address' => $peer['remote-address'],
+            'remote-as' => $peer['remote-as'],
+            'disabled' => $peer['disabled'],
+            'established' =>  isset($peer['established']) ? $peer['established'] : null,
+        ];
+    }
+}
+
+//Compare sessions
+foreach ($currentStates as $cur) {
+    if(
+        //Session latest exist end state was changed
+        isset($lastStates["{$cur['router']}{$cur['remote-address']}"]) &&
+        $lastStates["{$cur['router']}{$cur['remote-address']}"]['established'] != $cur['established']
+    ) {
+        sendNotify(
+            $lastStates["{$cur['router']}{$cur['remote-address']}"],
+            $cur,
+            $cur['established'] ? 'up' : 'down'
+        );
+    } elseif (
+        //New session detected
+        !isset($lastStates["{$cur['router']}{$cur['remote-address']}"])
+    ) {
+        sendNotify(
+            null,
+            $cur,
+            'peer-added'
+        );
+    }
+}
+foreach ($lastStates as $key=>$lastState) {
+    if(!isset($currentStates[$key])) {
+        sendNotify($currentStates[$key], null, 'peer-removed');
+    }
+}
+
+//Save current state
+file_put_contents(
+    __DIR__ . '/last_state.json',
+    json_encode(
+        array_values($currentStates),
+        JSON_PRETTY_PRINT
+    )
+);
+
+function sendNotify($old, $new, $eventType) {
+    switch ($eventType) {
+        case 'peer-added':
+            echo "Peer with name {$new['name']} will be created\n";
+            break;
+        case 'peer-removed':
+            echo "Peer with name {$old['name']} will be removed\n";
+            break;
+        case 'up':
+            echo "Peer with name {$new['name']} UP\n";
+            break;
+        case 'down':
+            echo "Peer with name {$new['name']} DOWN\n";
+            break;
+    }
+}
